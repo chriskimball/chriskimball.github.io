d7269a6b809f4b223cc5e38df82e31d95894e322
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-04-04 01:29:20 -0700

    Added banner image

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import '../styles/banner.css'
+
+const Banner = () => {
+  return (
+    <div class="banner">
+      <span role="img" aria-label="snowy lake basin at sunrise surrounded by jagged mountains"></span>
+      <div class="banner-subtitle">
+        <p>The mountains are calling and I must go.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
